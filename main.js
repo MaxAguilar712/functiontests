e61d671e0b593c00ed8537d1199f1126f6ecf4f7
MaxAguilar712 <a26001977@gmail.com>
2023-09-19 09:35:49 -0400

    :new main

---
+++ main.js
@@ -0,0 +1,46 @@
+function taco() {
+ console.log("hi")
+}
+const milkshakes = () => {
+ console.log("howdy")
+}
+
+taco()
+
+milkshakes()
+
+
+const makeAString = () => {
+
+    return makeAString
+}
+
+const storageVariable = makeAString()
+
+console.log(makeAString())
+
+const subtraction = (number01, number02) => {
+    return number01 - number02
+}
+const finalSum = subtraction(72712, 29782)
+console.log(finalSum)
+
+
+// const postToDom = () => {
+//     const htmlString = `<h1> I am an action Function</h1>`
+//     document.querySelector(".pickme").innerHTML = htmlString
+// }
+const postToDom = () => {
+    const htmlString = `<h1> ${finalSum} </h1>`
+    document.querySelector(".pickme").innerHTML = htmlString
+}
+postToDom()
+
+const addition = (number1, number2) => {
+    return number1 + number2
+}
+const totalsum = addition(77, 980)
+const postMathToDom = (totalsum)
+console.log(totalsum)
+
+
